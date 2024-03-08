cee4bc02649a573c8256b0505c1d23f503ac2609
Jamis Buck <jamisbuck@gmail.com>
2024-03-08 08:16:36 -0700

    bump to jdk17 (#59)

--- shlib/set_env.sh
+++ shlib/set_env.sh
@@ -1,39 +1,16 @@
 # When changing, also update the hash in share/Dockerfile.
 TOOLCHAIN_VERSION=ded7ea845b08cf96f11a747d9540ba3199580dea
+JDK_VERSION=jdk17
 
 set_env_java() {
   ls -l /opt || true
   ls -l /usr/lib/jvm || true
 
   # Use toolchain java if it exists
-  if [ -f /opt/java/jdk8/bin/java ]; then
-    export JAVACMD=/opt/java/jdk8/bin/java
-    #export PATH=$PATH:/opt/java/jdk8/bin
-  fi
-
-  # ppc64le has it in a different place
-  if test -z "$JAVACMD" && [ -f /usr/lib/jvm/java-1.8.0/bin/java ]; then
-    export JAVACMD=/usr/lib/jvm/java-1.8.0/bin/java
-    #export PATH=$PATH:/usr/lib/jvm/java-1.8.0/bin
-  fi
-
-  if true; then
-    # newer
-    # rhel71-ppc, https://jira.mongodb.org/browse/BUILD-9231
-    if test -z "$JAVACMD" &&
-      (ls /opt/java || true) |grep -q java-1.8.0-openjdk-1.8.0 &&
-      test -f /opt/java/java-1.8.0-openjdk-1.8.0*/bin/java;
-    then
-      path=$(cd /opt/java && ls -d java-1.8.0-openjdk-1.8.0* |head -n 1)
-      export JAVACMD=/opt/java/"$path"/bin/java
-    fi
+  if [ -f /opt/java/$JDK_VERSION/bin/java ]; then
+    export JAVACMD=/opt/java/$JDK_VERSION/bin/java
   else
-    # older
-    # rhel71-ppc seems to have an /opt/java/jdk8/bin/java but it doesn't work
-    if test -n "$JAVACMD" && ! exec $JAVACMD -version; then
-      JAVACMD=
-      # we will try the /usr/lib/jvm then
-    fi
+    echo Could not find $JDK_VERSION in /opt/java
   fi
 
   if test -n "$JAVACMD"; then
